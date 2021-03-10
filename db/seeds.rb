a54a24cdf915bccb239a709a440834a4a9806301
Nestor <nestor.venegas95@gmail.com>
2021-03-10 16:00:57 -0600

    oof

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ Monster.destroy_all
 Player.destroy_all
 Round.destroy_all
 
-plr1 = Player.create(name: "Ruby Da Cherry", lvl: 1, exp: 0, hp: 5, rubees: 0)
+player = Player.create(name: "Ruby Da Cherry", lvl: 1, exp: 0, hp: 5, rubees: 0, max_hp: 5, attack: "Punch")
 
 lvl1 = Level.create(name: "Wally World")
 
-mon1 = Monster.create(name: "Goblin", exp: 3, hp: 1, level_id: lvl_1.id)
+goblin = Monster.create(name: "Goblin", exp: 3, hp: 1, level_id: lvl1.id, max_hp: 1, attack: "Punch")
 
-ro1 = Round.create( player_id: plr1.id, monster_id: mon1.id)
+ro1 = Round.create( player_id: player.id, monster_id: goblin.id)
 
 
